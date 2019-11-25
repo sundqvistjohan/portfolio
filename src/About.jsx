4c38e4fa84868537b17e5ea13f5e9ed1eeef8c80
Johan Sundqvist <sundqvist.johan@outlook.com>
2019-11-25 21:22:03 +0100

    Adds About and Projects pages

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const About = () => {
+    return (
+        <div className="ui container">
+            <h1 className="ui header">About Me</h1>
+            <p>This is text about me. Lorem ipsum etc.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/Projects.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Projects = () => {
+    return (
+        <div className="ui container">
+            <h1 className="ui header">My Projects</h1>
+            <p>A paragraph about my projects.</p>
+        </div>
+    )
+}
+
+export default Projects
