c9f5313d299fc20c058b952939fd6a79c65f6c0b
Johan Sundqvist <sundqvist.johan@outlook.com>
2019-11-26 10:03:12 +0100

    Updates Projects to stateful class, reads projects from JSON file

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,12 +1,48 @@
-import React from "react"
+import React, { Component } from "react"
+import axios from "axios"
 
-const Projects = () => {
-    return (
-        <div className="ui container">
-            <h1 className="ui header">My Projects</h1>
-            <p>A paragraph about my projects.</p>
-        </div>
-    )
-}
+class Projects extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('./src/data/projects.json')
+            .then(response => {
+                this.setState({
+                    projects: response.data
+                })
+            })
+    }
+
+    render() {
+        const projects = this.state.projects
+        let projectsList
+
+        if (projects.length > 0) {
+            projectsList = projects.map(project => {
+                return (
+                    <div key={project.id}>
+                        <h3 className="ui header">
+                            {project.name}
+                        </h3>
+                    </div>
+                )
+            })
+        }
+
+        return (
+
+            <div className="ui main container">
+                <h1 className="ui header">
+                    {projectsList}
+                </h1>
+            </div>
+        )
+    }
+};
 
 export default Projects
